b9e4d290696f649e67bb7dec535f0565f6084012
Denise <104241788+ytidsao@users.noreply.github.com>
2023-04-27 12:41:53 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'439ff921-2952-4d40-9b89-15e1b3ba5d27
-'}
+UUID=${UUID:-'439ff921-2952-4d40-9b89-15e1b3ba5d27'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/439ff921-2952-4d40-9b89-15e1b3ba5d27-vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/439ff921-2952-4d40-9b89-15e1b3ba5d27-vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/439ff921-2952-4d40-9b89-15e1b3ba5d27-trojan'}
